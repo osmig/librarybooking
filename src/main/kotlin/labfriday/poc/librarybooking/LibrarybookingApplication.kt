8b0e342de009394a1a0019c1c568853c99b70169
Carl Frendin <carl.frendin@gmail.com>
2019-03-22 09:47:16 +0100

    Intial commit

---
+++ src/main/kotlin/labfriday/poc/librarybooking/LibrarybookingApplication.kt
@@ -0,0 +1,11 @@
+package labfriday.poc.librarybooking
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LibrarybookingApplication
+
+fun main(args: Array<String>) {
+    runApplication<LibrarybookingApplication>(*args)
+}
