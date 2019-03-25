fc015ff7124c46722f9e457d10a724191a0d1382
Carl Frendin <carl.frendin@gmail.com>
2019-03-25 08:47:30 +0100

    TMP: Todo, add JPA/persistence

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,8 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.h2database:h2")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
